cf87cc7c2bb32f7903af85626ed090add503c10b
joshua-smart <josh@thesmarts.co.uk>
2022-10-15 21:52:47 +0100

    Updated xmonad config

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -6,8 +6,6 @@ import XMonad.Util.Run
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Spacing
 import XMonad.Layout.Dwindle
-import qualified DBus as D
-import qualified DBus.Client as D
 import qualified Codec.Binary.UTF8.String as UTF8
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
@@ -21,6 +19,7 @@ import qualified Data.Map        as M
 -- ############################
 myTerminal = "alacritty"
 myBrowser = "firefox"
+myEditor = "nvim"
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
@@ -32,7 +31,7 @@ myBorderWidth = 0
 -- Set Windows key as modMask
 myModMask = mod4Mask
 -- Workspace names
-myWorkspaces = ["1", "2"]
+myWorkspaces = ["0", "1"]
 -- Border colors for unfocused and focused windows, respectively.
 myNormalBorderColor  = "#13161a"
 myFocusedBorderColor = "#7f4991"
@@ -44,6 +43,7 @@ myKeys conf@XConfig {XMonad.modMask = modm} = M.fromList $
     [ ((modm,               xK_Return               ), spawn myTerminal                                  ) -- Launch terminal
     , ((modm,               xK_numbersign           ), spawn myBrowser                                   ) -- Launch browser
     , ((modm,               xK_n                    ), spawn $ myTerminal ++ " -e node"                  ) -- Launch node in terminal
+    , ((modm,               xK_Shift_R              ), spawn $ myTerminal ++ " -e " ++ myEditor          ) -- Launch editor
     , ((modm,               xK_d                    ), spawn "rofi -show drun"                           ) -- Launch drun menu
     , ((modm,               xK_f                    ), spawn "rofi -show run"                            ) -- Launch run menu
     , ((modm,               xK_s                    ), spawn "rofi -show ssh"                            ) -- Launch ssh menu
@@ -68,9 +68,9 @@ myKeys conf@XConfig {XMonad.modMask = modm} = M.fromList $
     , ((modm,               xK_p                    ), spawn "xmonad --recompile; xmonad --restart"      ) -- Restart xmonad
     , ((0,                  xF86XK_MonBrightnessUp  ), spawn "brightnessctl s +10%"                      ) -- Increment brightness
     , ((0,                  xF86XK_MonBrightnessDown), spawn "brightnessctl s 10%-"                      ) -- Deincrement brightness
-    , ((0,                  xF86XK_AudioRaiseVolume ), spawn "pulseaudio-control --volume-max 110 up"    ) -- Increment volume
-    , ((0,                  xF86XK_AudioLowerVolume ), spawn "pulseaudio-control --volume-max 110 down"  ) -- Deincrement volume
-    , ((0,                  xF86XK_AudioMute        ), spawn "pulseaudio-control togmute"                ) -- Mute
+    , ((0,                  xF86XK_AudioRaiseVolume ), spawn "pactl set-sink-volume @DEFAULT_SINK@ +5%"  ) -- Increment volume
+    , ((0,                  xF86XK_AudioLowerVolume ), spawn "pactl set-sink-volume @DEFAULT_SINK@ -5%"  ) -- Deincrement volume
+    , ((0,                  xF86XK_AudioMute        ), spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle" ) -- Mute
     , ((modm,               xK_t                    ), spawn "polybar-msg action \"#date.toggle\""       ) -- Toggle date in statusbar
     , ((0,                  xK_Print                ), spawn "scrot ~/Images/screenshots/%Y-%m-%d-%T.png") -- Take screenshot
     , ((0,                  xF86XK_AudioPlay        ), spawn "playerctl play-pause"                      ) -- Play pause current media
@@ -102,13 +102,12 @@ myBorder i = Border i i i i
 mySpacing i = spacingRaw True (myBorder i') True (uniformBorder i') True
     where i' = fromIntegral i
 
-myLayout = avoidStruts (mySpacing 10 $ tiled ||| dwindle ||| Full)
+myLayout = avoidStruts (mySpacing 10 $ tiled ||| Full)
     where
     tiled   = Tall nmaster delta ratio -- default tiling algorithm partitions the screen into two panes
     nmaster = 1 -- The default number of windows in the master pane
     ratio   = 3/5 -- Default proportion of screen occupied by master pane
     delta   = 3/100 -- Percent of screen to increment by when resizing panes
-    dwindle = Spiral R CW 1.0 1.1 -- Fibonnaci spiral tiling
 
 -- ###########################
 -- #### WINDOW MANAGEMENT ####
@@ -139,6 +138,7 @@ myStartupHook = do
     spawnOnce "polybar primary &"
     spawnOnce "unclutter -idle 1 &"
     spawnOnce "udiskie &"
+    spawnOnce "nm-applet &"
 
 -- ##############
 -- #### MAIN ####
